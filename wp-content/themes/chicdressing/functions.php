bff7536457ee732a80a83e8f8245e52e79eeb401
Soulstorm <teddy.cozette@hotmail.fr>
2024-07-22 19:20:36 +0200

    hebergement des polices du site en local au lieu de les appeller depuis le site de google font

--- wp-content/themes/chicdressing/functions.php
+++ wp-content/themes/chicdressing/functions.php
@@ -7,7 +7,20 @@ function chicdressing_enqueue_styles() {
 
 add_filter( 'big_image_size_threshold', '__return_false' );
 
+/* On retire les polices appeller sur google font depuis le thème parent */
 
+function remove_google_fonts () {
+    wp_dequeue_style('ashe-playfair-font');
+
+    wp_dequeue_style('ashe-opensans-font');
+
+    wp_dequeue_style('ashe-kalam-font');
+
+    wp_dequeue_style('ashe-rokkitt-font');
+
+    wp_deregister_script('ashe_gfonts_scripts');
+}
+add_action('wp_enqueue_scripts', 'remove_google_fonts', 999);
 
 
  
